cc8c4ae6fde13a81c81f7b7f6e730c8c3334b3a7
adenugbamichael <adenugbamicke@gmail.com>
2023-11-07 18:43:04 +0100

    navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Lato } from "next/font/google"
 import "./globals.css"
+import Navbar from "./components/Navbar"
 
 const lato = Lato({ weight: "400", subsets: ["latin"] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={lato.className}>{children}</body>
+      <body className={lato.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero"
+
 export default function Home() {
   return (
     <div className='bg-white pb-6 sm:pb-8 lg:pb-12'>
-      <p>main page</p>
+      <Hero />
     </div>
   )
 }
