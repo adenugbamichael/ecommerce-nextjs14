224d7f0f207055e3dd9703c839c486820eca3985
adenugbamichael <adenugbamicke@gmail.com>
2023-11-10 17:06:23 +0100

    home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next"
-import { Lato } from "next/font/google"
+import { Merriweather_Sans } from "next/font/google"
 import "./globals.css"
 import Navbar from "./components/Navbar"
 
-const lato = Lato({ weight: "400", subsets: ["latin"] })
+const Merriweather = Merriweather_Sans({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={lato.className}>
+      <body className={Merriweather.className}>
         <Navbar />
         {children}
       </body>
---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,46 @@
+import ImageGallery from "@/app/components/imageGallery"
+import { fullProduct } from "@/app/interface"
+import { client } from "@/app/lib/sanity"
+
+async function getData(slug: string) {
+  const query = `
+    *[_type == "product" && slug.current == "${slug}"][0]{
+   _id, 
+     images,
+  price, 
+  name, 
+     description,
+  "slug": slug.current,
+    "categoryName": category->name
+}`
+
+  const data = await client.fetch(query)
+  return data
+}
+
+export default async function ProductPage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const data: fullProduct = await getData(params.slug)
+  return (
+    <div className='bg-white'>
+      <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
+        <div className='grid gap-8 md:grid-cols-2'>
+          <ImageGallery images={data.images} />
+          <div className='md:py-8'>
+            <div className='mb-2 md:mb-3'>
+              <span className='mb-0.5 inline-block text-gray-500'>
+                {data.categoryName}
+              </span>
+              <h2 className='text-2xl font-bold text-gray-800 lg:text-3xl'>
+                {data.name}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
